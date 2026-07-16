9b51de1c2fe1ef3989773bb2d11aad569c8b0b1c
gvijay00 <gurugubellivijayakumar@gmail.com>
2026-07-16 22:52:46 +0530

    news card assignment completed

--- core/src/main/java/com/newsportal/core/models/NewsCardModel.java
+++ core/src/main/java/com/newsportal/core/models/NewsCardModel.java
@@ -1,4 +1,64 @@
 package com.newsportal.core.models;
 
-public class a {
+import java.util.Date;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class NewsCardModel {
+
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String description;
+
+    @ValueMapValue
+    private String fileReference;
+
+    @ValueMapValue
+    private String category;
+
+    @ValueMapValue
+    private String author;
+
+    @ValueMapValue
+    private Date publishDate;
+
+    @ValueMapValue
+    private String readMorePage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFileReference() {
+        return fileReference;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public String getReadMorePage() {
+        return readMorePage;
+    }
 }
